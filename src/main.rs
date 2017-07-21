25c805b1b949ae47ff8b37ef33887043823a10fc
Thomas Zurkan <thomas.zurkan@optimizely.com>
2017-07-21 00:04:53 -0700

    add enviroment variables

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,44 @@ use maildirqueue::MaildirQueue;
 use jsonsender::JsonSender;
 
 fn main() {
-    let child1 = thread::spawn(move || {
-       startServer("."); 
-    });
-    let child2 = thread::spawn(move || {
-       startServer("."); 
-    });
-    let child3 = thread::spawn(move || {
-       startServer("."); 
-    });
-
-    startServer(".");
+    let mut base_dir = "".to_string();
+    let mut num_threads = 3;
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 {
+        println!("The first argument is {}", args[1]);
+        let last = args.len();
+        for x in 1..last {
+            println!("{}", x);
+            if x % 2 == 0 { continue; }    
+            let first = args[x].to_string();
+            let second = args[x+1].to_string();
+            match &*first {
+                "-threads" => num_threads = second.parse::<u32>().unwrap(),
+                "-baseDir" => {  base_dir.push_str(&*second); println!("{}", second); },
+                &_ => println!("wrong parameters use -threads 3 -baseDir queuedir"),
+            }
+        }
+    }
+   
+    if base_dir.len() == 0 {
+        base_dir.push_str(".");
+    }
+    println!("{}", num_threads);
+    println!("{}", base_dir);
+    let mut children = vec![];
+    for _ in 0..num_threads {
+        println!("start thread");
+        let working_dir = base_dir.clone().to_owned();
+        children.push(thread::spawn(move || {
+        startServer(working_dir.as_str()); 
+        }));
+    }
+    
+    println!("main thread");
+    for child in children {
+       // Wait for the thread to finish. Returns a result.
+       let _ = child.join();
+    }
 }
 
 fn startServer(baseDir:&str) {
